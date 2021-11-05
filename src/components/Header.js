f09fa96f3986f47141f683fe1372e64107fffabc
Pulasthi Bethmage <87483405+pulabethmage@users.noreply.github.com>
2021-11-05 07:04:37 +0530

    Fitst Commit

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+const Header = () => {
+  return (
+    <header className="header m-2">
+      <div class="row">
+        <div class="col-sm-8">
+          <h1>PMIS APP</h1>
+        </div>
+        <div class="col-sm-4">
+          <button className="btn btn-primary btn-sm">View</button>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
